033c9e8e45f22e6d1f73c92701220b18abd589ff
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2024-06-28 14:38:41 +0200

    initial setup

---
+++ src/payload/collections/Media.ts
@@ -0,0 +1,46 @@
+import { slateEditor } from '@payloadcms/richtext-slate'
+import path from 'path'
+import type { CollectionConfig } from 'payload/types'
+
+export const Media: CollectionConfig = {
+  slug: 'media',
+  upload: {
+    staticDir: path.resolve(__dirname, '../../../media'),
+    imageSizes: [
+      {
+        name: 'sc_550x550',
+        width: 550,
+        height: 550,
+      },
+      {
+        name: 'sc_1100x1100',
+        width: 1100,
+        height: 1100,
+      },
+      {
+        name: 'sc_1020x296',
+        width: 1020,
+        height: 296,
+      },
+    ],
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'alt',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'caption',
+      type: 'richText',
+      editor: slateEditor({
+        admin: {
+          elements: ['link'],
+        },
+      }),
+    },
+  ],
+}
